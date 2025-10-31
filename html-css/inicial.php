410d2f97e94cd0d5799a338c6636e68014cc1e4e
viniciusestevamsilva <viniciusestevamsilvajf@gmail.com>
2025-10-31 20:57:23 -0300

    FEAT: Implementação do sidebar via include

--- html-css/inicial.php
+++ html-css/inicial.php
@@ -18,26 +18,7 @@
 
     <!-- Conteúdo principal da página -->
     <main>
-        <!-- Sidebar lateral (drawer) -->
-        <aside class="sidebar" id="sidebar">
-            <!-- Botão que abre/fecha a sidebar (toggle) -->
-            <button class="botao" id="botao-abrefecha">☰</button>
-
-            <!-- Espaço reservado para a imagem do usuário -->
-            <img src="" alt="">
-
-            <!-- Lista de navegação da sidebar -->
-            <ul>
-                <!-- Cada item da lista tem um ícone (emoji) + link de navegação -->
-                <li>🏠<span><a href="inicial.php">Início</a></span></li>
-                <li>📄<span><a href="documentos.php">Solicitações</a></span></li>
-                <li>📧<span><a href="mensagens.php">Recibos</a></span></li>
-                <li>📧<span><a href="mensagens.php">Banco de Horas</a></span></li>
-                <li>📧<span><a href="mensagens.php">Cadastro de Funcionários</a></span></li>
-                <li>⚙️<span><a href="configuracoes.php">Configurações</a></span></li>
-                <li>⚙️<span><a href="configuracoes.php">Sair</a></span></li>
-            </ul>
-        </aside>
+        <?php include "./sidebar.php" ?>
 
         <!-- Área de conteúdo principal (fora da sidebar) -->
         <article class="article-tela-inicial" role="list">
---
+++ html-css/sidebar.php
@@ -0,0 +1,20 @@
+        <!-- Sidebar lateral (drawer) -->
+        <aside class="sidebar" id="sidebar">
+            <!-- Botão que abre/fecha a sidebar (toggle) -->
+            <button class="botao" id="botao-abrefecha">☰</button>
+
+            <!-- Espaço reservado para a imagem do usuário -->
+            <img src="" alt="">
+
+            <!-- Lista de navegação da sidebar -->
+            <ul>
+                <!-- Cada item da lista tem um ícone (emoji) + link de navegação -->
+                <li><img src="../html-css/public/img/Cerebro.ico"><span><a href="inicial.php">Início</a></span></li>
+                <li>📄<span><a href="documentos.php">Solicitações</a></span></li>
+                <li>📧<span><a href="mensagens.php">Recibos</a></span></li>
+                <li>📧<span><a href="mensagens.php">Banco de Horas</a></span></li>
+                <li>📧<span><a href="mensagens.php">Cadastro de Funcionários</a></span></li>
+                <li>⚙️<span><a href="configuracoes.php">Configurações</a></span></li>
+                <li>⚙️<span><a href="configuracoes.php">Sair</a></span></li>
+            </ul>
+        </aside>
