600a25a9097e1e2880545bbada8957a86f76d18d
DianLuca <dianlucca@hotmail.com>
2025-12-16 15:27:17 -0300

    FEAT: Ajustes e reorganização dos nomes de arquivos. Correção do design para a abertura e fechamento do menu lateral, bem como o cabeçalho da página.

--- html-css/sidebar.php
+++ html-css/sidebar.php
@@ -1,21 +1,21 @@
-        <!-- Sidebar lateral (drawer) -->
-        <aside class="sidebar" id="sidebar">
-            <!-- Botão que abre/fecha a sidebar (toggle) -->
-            <button class="botao" id="botao-abrefecha">☰</button>
+<!-- Sidebar lateral (drawer) -->
+<aside class="sidebar open" id="sidebar">
+    <!-- Botão que abre/fecha a sidebar (toggle) -->
+    <button class="botao" id="botao-abrefecha">☰</button>
 
-            <!-- Espaço reservado para a imagem do usuário -->
-            <img src="" alt="">
+    <!-- Espaço reservado para a imagem do usuário -->
+    <img class='avatar' src="../html-css/public/img/Avatar.png" alt="avatar">
 
-            <!-- Lista de navegação da sidebar -->
-            <ul>
-                <!-- Cada item da lista tem um ícone (emoji) + link de navegação -->
-                <li><img src="../html-css/public/img/PlaceHolder"><span><a href="inicial.php">Início</a></span></li>
-                <li><img src="/public/img/PlaceHolder"><span><a href="documentos.php">Solicitações</a></span></li>
-                <li><img src="../html-css/public/img/PlaceHolder"><span><a href="mensagens.php">Recibos</a></span></li>
-                <li><img src="../html-css/public/img/PlaceHolder"><span><a href="mensagens.php">Banco de Horas</a></span></li>
-                <li><img src="../html-css/public/img/PlaceHolder"><span><a href="mensagens.php">Cadastro de Funcionários</a></span></li>
-                <li><img src="../html-css/public/img/PlaceHolder"><span><a href="configuracoes.php">Configurações</a></span></li>
-                <li><img src="../html-css/public/img/PlaceHolder"><span><a href="configuracoes.php">Sair</a></span></li>
+    <!-- Lista de navegação da sidebar -->
+    <ul>
+        <!-- Cada item da lista tem um ícone (emoji) + link de navegação -->
+        <li><a href="inicial.php"><img src="../html-css/public/img/icones/inicio.png"><span>Início</span></a></li>
+        <li><a href="solicitacoes.php"><img src="../html-css/public/img/icones/solicitacoes.png"><span>Solicitações</span></a></li>
+        <li><a href="pagamento.php"><img src="../html-css/public/img/icones/pagamentos.png"><span>Recibos</span></a></li>
+        <li><a href="controle_de_ponto.php"><img src="../html-css/public/img/icones/pontos.png"><span>Banco de Horas</span></a></li>
+        <li><a href="cadastro_de_funcionario.php"><img src="../html-css/public/img/icones/funcionarios.png"><span>Funcionários</span></a></li>
+        <li><a href="configuracoes.php"><img src="../html-css/public/img/icones/configuracoes.png"><span>Configurações</span></a></li>
+        <li><a href="tela_login.php" class="botao-sair">SAIR</a></li>
 
-            </ul>
-        </aside>
+    </ul>
+</aside>
