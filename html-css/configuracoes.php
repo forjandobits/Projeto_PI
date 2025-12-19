05e28baa4b94b8f27118e7e5b9284b98cc232b8c
DianLuca <dianlucca@hotmail.com>
2025-12-19 20:33:24 -0300

    FEAT: Página de configurações com alterações aplicada. Novas imagens para compor as configurações adicionadas. Pequenas alterações alterações de estilo aplicadas.

--- html-css/configuracoes.php
+++ html-css/configuracoes.php
@@ -5,76 +5,66 @@
         <article class="cabecalhos">
             <h1>Configurações</h1>
         </article>
-        <article class="article-configuracoes">
-            <h2>Configurações</h2>
+        <article>
 
-            <form action="" id="form-configuracoes">
-                <article>
-                    <section class="section-tema-configuracoes">
-                        <div>
+            <form action="" id="configuracoes">
+                <section class="areas-form">
+                    <div class="grupo-campo">
+                        <div class="campo resumo">
+                            <label for="tema-claro">Tema Claro:</label>
                             <input type="radio" name="tema" id="tema-claro">
-                            <label for="tema-claro">
-                                <h4>Tema Claro:</h4>
-                            </label>
                         </div>
-                        <div>
-                            <img src="public/img/Tela Original Configurações.png" alt="exemplo tema claro">
-                        </div>
-                    </section>
-                    <section class="section-tema-configuracoes">
-                        <div>
+                        <img src="public/img/temas/Tela_Padrao_Configurações.png" alt="Tela com tema claro">
+                    </div>
+                
+                    <div class="grupo-campo">
+                        <div class="campo resumo">
+                            <label for="tema-escuro">Tema Escuro:</label>
                             <input type="radio" name="tema" id="tema-escuro">
-                            <label for="tema-escuro">
-                                <h4>Tema Escuro:</h4>
-                            </label>
                         </div>
-                        <div>
-                            <img src="public/img/Tela Preta Configurações.png" alt="exemplo tema escuro">
-                        </div>
-                    </section>
-                </article>
-                <article>
-                    <section class="section-tema-configuracoes">
-                        <div>
+                        <img src="public/img/temas/Tela_Escura_Configurações.png" alt="Tela com tema escuro">
+                    </div>
+                    
+                    <div class="grupo-campo">
+                        <div class="campo resumo">
+                            <label for="tema-contraste">Alto Contraste:</label>
                             <input type="radio" name="tema" id="tema-contraste">
-                            <label for="tema-contraste">
-                                <h4>Alto Contraste:</h4>
-                            </label>
                         </div>
-                        <div>
-                            <img src="public/img/Tela Contraste Configurações.png" alt="exemplo de alto contraste">
-                        </div>
-                    </section>
-                    <section class="section-fontes-configuracoes">
-                        <h4>Tamanho da Fonte:</h4>
-                        <div>
-                            <button id="fonte-menor">Aa</button>
-                            <button id="fonte-media">Aa</button>
-                            <button id="fonte-maior">Aa</button>
-                        </div>
-                    </section>
-                </article>
-                <article>
-                    <section class="section-alterar-senha-configuracoes">
-                        <h4>Alterar Senha:</h4>
-                        <div>
-                            <div class="alterar-senha-campo-configuracoes">
-                                <label for="senha-atual">Senha Atual:</label>
-                                <input type="text" id="senha-atual" name="senha-atual">
-                            </div>
-                            <div class="alterar-senha-campo-configuracoes">
-                                <label for="nova-senha">Nova Senha:</label>
-                                <input type="text" id="nova-senha" name="nova-senha">
-                            </div>
-                            <div class="alterar-senha-campo-configuracoes">
-                                <label for="repita-senha">Repita Nova Senha:</label>
-                                <input type="text" id="repita-senha" name="repita-senha">
-                            </div>
+                        <img src="public/img/temas/Tela_Contraste_Configurações.png" alt="Tela com alto contraste">
+                    </div>
+                </section>
+                <section class="areas-form">
+        
+                    <div class="campo opcoes-tamanho">
+                        <label>Tamanho da Fonte:</label>
+                        <button id="fonte-menor">Diminuir</button>
+                        <button id="fonte-media">Padrão</button>
+                        <button id="fonte-maior">Aumentar</button>
+                    </div>
+                    <!-- <div class="grupo-campo">
+                    </div> -->
+                </section>
+            </form>
 
-                        </div>
+            <form>
+                <section class="areas-form">
+                    <h2>Alterar Senha:</h2>
+                    <div class="campo">
+                        <label for="senha-atual">Senha Atual:</label>
+                        <input type="text" id="senha-atual" name="senha-atual">
+                    </div>
+                    <div class="campo">
+                        <label for="nova-senha">Nova Senha:</label>
+                        <input type="text" id="nova-senha" name="nova-senha">
+                    </div>
+                    <div class="campo">
+                        <label for="repita-senha">Repita Nova Senha:</label>
+                        <input type="text" id="repita-senha" name="repita-senha">
+                    </div>
+                    <div class="campo">
                         <button type="submit" id="btn-salvar-senha">Salvar Senha</button>
-                    </section>
-                </article>
+                    </div>
+                </section>
             </form>
         </article>
     </main>
