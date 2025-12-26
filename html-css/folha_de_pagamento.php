7e26b818067e1e954753ab76b87a32a5869417f3
DianLuca <dianlucca@hotmail.com>
2025-12-26 15:41:18 -0300

    FEAT: Ajustes para a visualização da folha de pagamento

--- html-css/folha_de_pagamento.php
+++ html-css/folha_de_pagamento.php
@@ -3,88 +3,93 @@
 <?php include "./sidebar.php" ?>
 
 <main>
-  <section class="movimentacao">
-    <h1>Folha de Pagamento</h1>
-  
-    <div class="inputs-mov">
-        <form id="filtro-funcionario">
-          <label for="filtro">Funcionário:</label>
-          <input type="text" id="funcionario">
-        </form>
-  
-        <form id="filtro-calendario">
+  <article class="cabecalhos">
+    <h1>Folha de Pagamento -Visualização</h1>
+  </article>
+
+  <!-- <article>
+    <form id="busca-pagamentos">
+      <section class="areas-form">
+        <div class="campo">
+          <label for="nome">Funcionário:</label>
+          <input type="text" id="nome">
+        </div>
+        <div class="campo">
           <label for="data">Mês Referente:</label>
-          <input type="date" id="data-calendario">
-        </form>
-    </div>
-  </section>
-  
-  
-  
-  <table>
-    <thead>
-      <tr>
-        <th>Cod.</th>
-        <th>Evento</th>
-        <th>Referência</th>
-        <th>Vencimentos</th>
-        <th>Descontos</th>
-      </tr>
-    </thead>
-  
-    <tbody>
-      <tr>
-        <td><strong>00</strong></td>
-        <td><strong>Salário Base</strong></td>
-        <td>2.000,00</td>
-        <td>2.000,00</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td><strong>01</strong></td>
-        <td><strong>Comissão</strong></td>
-        <td></td>
-        <td></td>
-        <td></td>
-      </tr>
-      <tr>
-        <td><strong>02</strong></td>
-        <td><strong>INSS</strong></td>
-        <td>10,88</td>
-        <td></td>
-        <td>0,81</td>
-      </tr>
-      <tr>
-        <td><strong>03</strong></td>
-        <td><strong>Repouso Remuneração</strong></td>
-        <td>0,00</td>
-        <td></td>
-        <td></td>
-      </tr>
-      <tr>
-        <td><strong>04</strong></td>
-        <td><strong>FGTS</strong></td>
-        <td>27,50</td>
-        <td></td>
-        <td></td>
-      </tr>
-      <tr class="linha-totais">
-        <td colspan="2" ></td>   <!-- pula Cod. e Evento -->
-        <td class="totais-branco"><strong>Totais</strong></td>
-        <td>2.000,00</td>
-        <td>0,81</td>
-      </tr>
+          <input type="date" id="data">
+        </div>
+      </section>
+    </form>
+  </article> -->
+  <article>
+
+    <table>
+      <caption>Folha de Pagamento - Mês/Ano(?) - Nome Funcionário(?)</caption>
+      <thead>
+        <tr>
+          <th>Código de Referência</th>
+          <th>Evento</th>
+          <th>Referência</th>
+          <th>Vencimentos</th>
+          <th>Descontos</th>
+        </tr>
+      </thead>
     
-    </tbody>
-  </table>
+      <tbody>
+        <tr>
+          <td>00</td>
+          <td>Salário Base</td>
+          <td>2.000,00</td>
+          <td>2.000,00</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>01</td>
+          <td>Comissão</td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>02</td>
+          <td>INSS</td>
+          <td>10,88</td>
+          <td></td>
+          <td>0,81</td>
+        </tr>
+        <tr>
+          <td>03</td>
+          <td>Descanso Remunerado</td>
+          <td>0,00</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>04</td>
+          <td>FGTS</td>
+          <td>27,50</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td colspan="2"></td>   <!-- pula Cod. e Evento -->
+          <td>Totais</td>
+          <td>2.000,00</td>
+          <td>0,81</td>
+        </tr>
+      
+      </tbody>
+    </table>
+
+    <section class="resumo-final">
+          <p>Total Líquido (R$): 2000,00</p>
+          <button>Editar(?)</button>
+          <button>Concluir</button>
+    </section>
+  </article>
   
-  <div class="resumo-final">
-      <span class="liquido">
-          <strong>Líquido</strong>: <span class="valor-liquido">2.000,00</span>
-      </span>
   
-      <button>Concluir</button>
-  </div>
+  
 </main>
 
 
