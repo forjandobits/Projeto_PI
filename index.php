043d2e817b2135b2858c9788aad23eb37b7aeaca
viniciusestevamsilva <viniciusestevamsilvajf@gmail.com>
2026-02-11 21:42:36 -0300

    FEAT: Tela de login e conexão com o bd

--- index.php
+++ index.php
@@ -41,6 +41,13 @@
               <button type="submit">Entrar</button>
               <p id='erro' style='display:none;color:red'></p>
             </form>
+            <!-- Alterei os ids dos inputs para serem compativeis com o bd, e tambem coloquei o 'action' no form -->
+            <form class="form-modal-login" id="form-login" action="public/js/login-e-Configuracao/processa_login.php" method="POST">
+              <input type="text" id="usuario" name="usuario" placeholder="Usuário">
+              <input type="password" id="senha" name="senha" placeholder="Senha">
+              <button type="submit">Entrar</button>
+              <p id='erro' style='display:none;color:red'></p>
+            </form>
         </section>
 
       </article>
