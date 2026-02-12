774d101c0914ad8201aaf4b8e10fe36c976b3de2
AgataQuadros <quadrosagata@gmail>
2026-02-11 21:02:47 -0300

    FEAT: Iniciando o desenvolvimento das validações, Val iniciada = Nome Vazio

--- html-css/public/js/pagamentos/validacoes.js
+++ html-css/public/js/pagamentos/validacoes.js
@@ -11,63 +11,84 @@
 // esta nesse arquivo coo referencia de nada, 
 // a não ser que esteja especificado como seguro para tal
 
+document.addEventListener("DOMContentLoaded", () => {
+    const nome = getElementById("nome");
+    const mesano = getElementById("data-mes-ano");
+    const adevent = getElementById("adicionar-evento");
+    const savdados = getElementById("lancar-dados");
 
-function nomeVazio() {
-// se entrada de referencia para nome do funcionario estiver vazia, envie mensagem de erro
-// ativação = input
+    savdados.addEventListener("click", () => {
+        nomeVazio(nome);
+    });
+})
 
+
+function nomeVazio(nome) {
+    // se entrada de referencia para nome do funcionario estiver vazia, envie mensagem de erro
+    // ativação = input
+    alert("AAAAAAAA");
+    if (nome == "") {
+        mostrarMensagem("Você não pode deixar esse campo vazio!!");
+
+    }
 }
 
 
 function valorVazio() {
-// se qualquer das entradas pedindo um valor monetario estiver vazia, envie uma menssagem de erro
-// ativação = input
+    // se qualquer das entradas pedindo um valor monetario estiver vazia, envie uma menssagem de erro
+    // ativação = input
 
 }
 
 
 function nomeExiste() {
-// se o nome digitado na aba "nome" não existir no bd, enviar uma menssagem de erro
-// ativação = input
+    // se o nome digitado na aba "nome" não existir no bd, enviar uma menssagem de erro
+    // ativação = input
 
 }
 
 
 function evenSelec() {
-// se o evento ja foi selecionado na folha em registro ele não pode mais ser selecionado
-// ativação = select
+    // se o evento ja foi selecionado na folha em registro ele não pode mais ser selecionado
+    // ativação = select
 
 }
 
 
 function folhaVazia() {
-// se tentarem salvar a folha enquanto não tem nada nela, enviar menssagem de erro
-// ativação = click
+    // se tentarem salvar a folha enquanto não tem nada nela, enviar menssagem de erro
+    // ativação = click
 
 }
 
 
 function dataVazia() {
-// se a data estiver vazia, enviar uma menssagem de erro
-// ativação = input
+    // se a data estiver vazia, enviar uma menssagem de erro
+    // ativação = input
 
 }
 
 
 function datasDistantes() {
-// se a data que for introduzida for muito distante da data atual, enviar uma menssagem de confirmação
-// ativação = click
+    // se a data que for introduzida for muito distante da data atual, enviar uma menssagem de confirmação
+    // ativação = click
 
 }
 
-function sairSalvar(){
-// se o botão de sair o pop-up for clicado, enviar uma menssagem de confirmação
-// ativação = click
+function sairSalvar() {
+    // se o botão de sair o pop-up for clicado, enviar uma menssagem de confirmação
+    // ativação = click
 
 }
 
+function mostrarMensagem(texto) {
+    const resultado = document.getElementById('resultado');
+    resultado.textconten = texto;
+    resultado.style.color = '#FF0000';
+}
 
-nomeVazio();
+
+
 valorVazio();
 nomeExiste();
 evenSelec();
