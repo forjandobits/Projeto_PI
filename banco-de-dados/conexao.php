efae2a55937334b5b75a68f0fe90e6fc60920ce3
Isis França <166769666+FrancaIsis@users.noreply.github.com>
2026-02-20 00:28:19 -0300

    FEAT: Adaptação listar funcionarios com atualização do BD, inclusao da exibição da situacao (excedeu horas..)

--- banco-de-dados/conexao.php
+++ banco-de-dados/conexao.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$bd = "bd_humanamente_dados";// importante
+$bd = "bd_humanamente_teste";// importante
 $bd_existe = false;
 $caminho_bd = "../banco-de-dados/bd_humanamente_teste.sql";
 
