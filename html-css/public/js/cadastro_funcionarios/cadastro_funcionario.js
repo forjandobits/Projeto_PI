7b54de5f8af3a9b186d804a5fabee81d231084b0
leobarbosadev <leobarbosajf@hotmail.com>
2026-02-23 21:39:30 -0300

    Exibindo funcionarios

--- html-css/public/js/cadastro_funcionarios/cadastro_funcionario.js
+++ html-css/public/js/cadastro_funcionarios/cadastro_funcionario.js
@@ -1,14 +1,30 @@
-document.addEventListener('DOMContentLoaded', function(){
+// document.addEventListener('DOMContentLoaded', function(){
     async function listarFuncionarios() {
         const resposta = await fetch("public/js/cadastro_funcionarios/resposta.php");
         const funcionarios = await resposta.json();
-        
 
-    return funcionarios;
-}
+        const tabelaFuncionario = document.querySelector('#tabela-saida-colaboradores');
+        if(tabelaFuncionario){
+            tabelaFuncionario.innerHTML = "";
+
+            funcionarios.forEach(funcionario =>{
+                const novaCelulaFuncionario = tabelaFuncionario.insertRow();
+                console.log(funcionario);
+
+                const nome = novaCelulaFuncionario.insertCell();
+                const cargo = novaCelulaFuncionario.insertCell();
+                const situacao = novaCelulaFuncionario.insertCell();
+                const desligar = novaCelulaFuncionario.insertCell();
+                const visualizar = novaCelulaFuncionario.insertCell();
+
+                nome.textContent = funcionario.nome_completo;
+                cargo.textContent = funcionario.nome_cargo;
+                situacao.textContent = funcionario.situacao;
+                desligar.innerHTML = "<button>Desligar</button>";
+                visualizar.innerHTML = "<button class='abrir-modal'>Visualizar</button>";
+            });
+        }
+    }
 
 listarFuncionarios();
-})
-
-
-
+// })
