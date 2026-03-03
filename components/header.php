01697ecee6e768ccabcd7cd58ea536e0c789119c
BrendonJoaoDEV <brendonjoaodev@gmail.com>
2026-03-03 19:19:34 -0300

    FIX: Correção dos caminhos das imagens e dos estilos nos componentes

--- components/header.php
+++ components/header.php
@@ -4,19 +4,20 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Humanamente</title>
-    <link rel="stylesheet" href="../public/css/estilo.css" />
-    <link rel="stylesheet" href="../public/css/sidebar.css">
-    <link rel="stylesheet" href="../public/css/tabelas.css">
-    <link rel="stylesheet" href="../public/css/formularios.css">
-    <link rel="stylesheet" href="../public/css/correcoes.css">
-    <link rel="stylesheet" href="../public/css/modal.css">
+    <link rel="stylesheet" href="public/css/estilo.css" />
+    <link rel="stylesheet" href="public/css/sidebar.css">
+    <link rel="stylesheet" href="public/css/tabelas.css">
+    <link rel="stylesheet" href="public/css/formularios.css">
+    <link rel="stylesheet" href="public/css/correcoes.css">
+    <link rel="stylesheet" href="public/css/modal.css">
 
-    <link rel="shortcut icon" href="../public/img/Cerebro.ico" type="image/x-icon"/>
-    <script src="../public/js/sidebar.js"></script>
+    <link rel="shortcut icon" href="public/img/Cerebro.ico" type="image/x-icon"/>
+    <script src="public/js/sidebar.js"></script>
 </head>
 <body>
     <header>
-        <img src="../public/img/Logo_HM_Cerebro.png" alt="logo humanamente">
+        <img src="public/img/Logo_HM_Cerebro.png" alt="logo humanamente">
+        <button id="btnTeste">Teste</button>
     </header>
 
 <!-- Dessa forma é possível apenas com o "include" chamar todas as configurações passadas 
