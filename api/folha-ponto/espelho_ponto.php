3af6a8644ad1c0c2cb7663447fca925a9d80170a
Isis França <166769666+FrancaIsis@users.noreply.github.com>
2026-03-18 23:47:16 -0300

    FEAT: Correção filtro mes espelho ponto

--- api/folha-ponto/espelho_ponto.php
+++ api/folha-ponto/espelho_ponto.php
@@ -1,9 +1,12 @@
 <?php
+
+
 header("Content-Type: application/json");
 
 require_once __DIR__ . "/../../banco-de-dados/conexao.php";
 
 $requisicao = json_decode(file_get_contents("php://input"), true);
+
 
 if (!$requisicao || !isset($requisicao["id"])) {
     echo json_encode(["status" => "erro", "resposta" => "requisição inválida"]);
@@ -11,10 +14,22 @@ if (!$requisicao || !isset($requisicao["id"])) {
 }
 
 $id = (int)$requisicao["id"];
+$mes = $requisicao["mes"] ?? date("Y-m");
+$dataInicio = $mes . "-01";
+$dataFim = date("Y-m-d", strtotime("$dataInicio +1 month"));
 
-$sql = "SELECT * FROM view_espelho_ponto WHERE id_funcionario = ?";
+
+
+$sql = "SELECT * 
+        FROM view_espelho_ponto 
+        WHERE id_funcionario = ?
+        AND data >= ?
+        AND data < ?";
+
+
+
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("i", $id);
+$stmt->bind_param("iss", $id, $dataInicio, $dataFim);
 
 if ($stmt->execute()) {
     $resultado = $stmt->get_result();
@@ -24,8 +39,10 @@ if ($stmt->execute()) {
         $dados[] = $row;
     }
 
+    // echo json_encode($dados);
     echo json_encode(["status" => "sucesso", "resposta" => $dados]);
 } else {
     echo json_encode(["status" => "erro", "resposta" => "não foi possível executar a consulta sql"]);
 }
+
 ?>
