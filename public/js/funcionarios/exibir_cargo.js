3a0019165fbe4f266436d0fa71067f4427974bc4
leobarbosadev <leobarbosajf@hotmail.com>
2026-03-22 22:47:14 -0300

    FEAT: puxando somente o último dado

--- public/js/funcionarios/exibir_cargo.js
+++ public/js/funcionarios/exibir_cargo.js
@@ -27,9 +27,56 @@ document.addEventListener('DOMContentLoaded', function () {
                 escala.textContent = cargo.escala;
 
 
-                editar.innerHTML = `<button class='editar' id='${id_cargo}'>Editar</button>`;
+                editar.innerHTML = `<button class='abrir-modal' id='${id_cargo}'>Editar</button>`;
             })
         }
     }
+
+    async function exibiInformacoes(){
+        let idCargo;
+        document.addEventListener("click", async function(e) {
+            
+            if (e.target.classList.contains("abrir-modal")){
+                const exibir = document.querySelector(".modal");
+                exibir.style.display = "flex";
+
+                const travarCampos = exibir.querySelectorAll("input, select");
+
+                travarCampos.forEach(campos => {
+                    campos.disabled = true;
+                });
+
+                const respotaCargo = await fetch(`${BASE_URL}/api/funcionarios/exibir_cargo.php`, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        id_cargo: e.target.id
+                    })
+                })
+
+                idCargo = e.target.id;
+
+                const dadosCargo = await respotaCargo.json();
+                const cbo = document.querySelector('#cbo')
+
+
+                dadosCargo.forEach(dados =>{
+                    cbo.value = dados.cbo;
+                })
+            }
+            const botaoEditar = e.target.closest(".botao-editar");
+
+            if (botaoEditar) {
+
+                e.preventDefault();
+                window.location.href = `./cargos?id=${idCargo}`;
+            }
+        });
+        
+    }
+
     listarCargos();
+    exibiInformacoes();
 })
