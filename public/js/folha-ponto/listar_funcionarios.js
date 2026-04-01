9a35e0e9c49c4f5c71bea70bf182f756351735ff
BrendonJoaoDEV <brendonjoaodev@gmail.com>
2026-03-31 21:41:55 -0300

    REFAC: Começo da correção na filtragem de pontos

--- public/js/folha-ponto/listar_funcionarios.js
+++ public/js/folha-ponto/listar_funcionarios.js
@@ -1,59 +1,64 @@
 import { enviar } from "../utils/enviar.js";
 
-enviar(`${BASE_URL}./api/folha-ponto/listar_funcionarios.php`, {})
-    .then(dados => {
+document.addEventListener("DOMContentLoaded", async () => {
+    const filtro = document.querySelector("#filtro");
+    const dados = await enviar(`${BASE_URL}./api/folha-ponto/listar_funcionarios.php`, {});
 
-        const tbody = document.querySelector("#tabela-saida-ponto");
+    listarPontos(dados);
 
-        tbody.innerHTML = "";
-
-        dados.forEach(funcionario => {
-
-
-            const linha = document.createElement("tr");
-
-            const colunaNome = document.createElement("td");
-            colunaNome.textContent = funcionario.nome_completo;
-
-            const colunaBanco = document.createElement("td");
-            colunaBanco.textContent = funcionario.saldo_mes;
-
-            const colunaSituacao = document.createElement("td");
-            colunaSituacao.textContent = funcionario.situacao;
-
-            if (parseInt(funcionario.saldo_mes) < 0) {
-                colunaBanco.style.color = "red";
-            }
-            const colunaBotao = document.createElement("td");
-
-            //const link = document.createElement("a");
-            //link.href = "espelho_de_ponto.php?id=" + funcionario.id_funcionario;
-
-            const botao = document.createElement("button");
-            botao.textContent = "Visualizar";
-
-            botao.addEventListener("click", () => {
-                verEspelho(funcionario.id_funcionario);
-
-            })
-
-
-            colunaBotao.appendChild(botao);
-
-            linha.appendChild(colunaNome);
-            linha.appendChild(colunaBanco);
-            linha.appendChild(colunaSituacao);
-            linha.appendChild(colunaBotao);
-
-
-            tbody.appendChild(linha);
-
-        });
-
+    filtro.addEventListener("input", () => {
+        let pesquisa = filtro.ariaValueMax.toLowerCase();
 
     })
-    .catch(erro => console.error("Erro:", erro));
+
+});
 
 function verEspelho(id) {
     window.location.href = `espelho_de_ponto.php?id=${id}`;
 };
+
+function listarPontos(dados) {
+    const tbody = document.querySelector("#tabela-saida-ponto");
+
+    tbody.innerHTML = "";
+
+    dados.forEach(funcionario => {
+
+
+        const linha = document.createElement("tr");
+
+        const colunaNome = document.createElement("td");
+        colunaNome.textContent = funcionario.nome_completo;
+
+        const colunaBanco = document.createElement("td");
+        colunaBanco.textContent = funcionario.saldo_mes;
+
+        const colunaSituacao = document.createElement("td");
+        colunaSituacao.textContent = funcionario.situacao;
+
+        if (parseInt(funcionario.saldo_mes) < 0) {
+            colunaBanco.style.color = "red";
+        }
+        const colunaBotao = document.createElement("td");
+
+        const botao = document.createElement("button");
+        botao.textContent = "Visualizar";
+
+        botao.addEventListener("click", () => {
+            verEspelho(funcionario.id_funcionario);
+
+        })
+
+
+        colunaBotao.appendChild(botao);
+
+        linha.appendChild(colunaNome);
+        linha.appendChild(colunaBanco);
+        linha.appendChild(colunaSituacao);
+        linha.appendChild(colunaBotao);
+
+
+        tbody.appendChild(linha);
+
+    });
+}
